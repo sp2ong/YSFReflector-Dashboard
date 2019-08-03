69ad34725d999a0a2305c228c63d98fa8199a75f
Waldek <sp2ong@wp.pl>
2019-08-03 11:18:32 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+# This is an auto-generated config-file!
+# Be careful, when manual editing this!
+
+date_default_timezone_set('UTC');
+define("YSFREFLECTORLOGPATH", "/var/log/YSFReflector");
+define("YSFREFLECTORLOGPREFIX", "YSFReflector");
+define("YSFREFLECTORINIPATH", "/opt/YSFReflector");
+define("YSFREFLECTORINIFILENAME", "YSFReflector.ini");
+define("YSFREFLECTORPATH", "/opt/YSFReflector");
+define("TIMEZONE", "Europe/Warsaw");
+define("LOGO", "/dashboard/hblink.png");
+define("REFRESHAFTER", "60");
+define("SHOWOLDMHEARD", "60");
+define("SHOWPROGRESSBARS", "on");
+define("TEMPERATUREHIGHLEVEL", "60");
+define("SHOWQRZ", "on");
+?>
