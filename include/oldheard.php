6af8efefda9eb9cc0d6e5ce31ee5069f71fc5916
Waldek <sp2ong@wp.pl>
2019-10-02 15:50:08 +0200

    Update oldheard.php

--- include/oldheard.php
+++ include/oldheard.php
@@ -40,7 +40,7 @@ for ($i = 0; $i < count($oldallHeard); $i++) {
   </div>
 <script>
     $(document).ready(function(){
-      $('#oldheard').dataTable( {
+      $('#oldallHeard').dataTable( {
         language: {
     "sProcessing":   "Przetwarzanie...",
     "sLengthMenu":   "Pokaż _MENU_ pozycji",
