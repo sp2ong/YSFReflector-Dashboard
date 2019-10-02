91b06df8cc3139a214f86e8af71433dc1559b652
Waldek <sp2ong@wp.pl>
2019-10-02 13:46:57 +0200

    Update oldheard.php

--- include/oldheard.php
+++ include/oldheard.php
@@ -38,20 +38,35 @@ for ($i = 0; $i < count($oldallHeard); $i++) {
   </tbody>
   </table>
   </div>
-  <script>
-    $(document).ready(function(){
-      $('#oldallHeard').dataTable( {
-	        <script>
-    $(document).ready(function(){
-      $('#lh').dataTable( {
-        language: { url: 'http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Polish.json'},
-        "aaSorting": [[0,'desc']]
-      } );
-    });
-   </script>
-
-        "aaSorting": [[0,'desc']]
-      } );
-    });
-   </script>
+<script>
+    $(document).ready(function(){
+      $('#lh').dataTable( {
+        language: {
+    "sProcessing":   "Przetwarzanie...",
+    "sLengthMenu":   "Pokaż _MENU_ pozycji",
+    "sZeroRecords":  "Nie znaleziono pasujących pozycji",
+    "sInfoThousands":  " ",
+    "sInfo":         "Pozycje od _START_ do _END_ z _TOTAL_ łącznie",
+    "sInfoEmpty":    "Pozycji 0 z 0 dostępnych",
+    "sInfoFiltered": "(filtrowanie spośród _MAX_ dostępnych pozycji)",
+    "sInfoPostFix":  "",
+    "sSearch":       "Szukaj:",
+    "sUrl":          "",
+    "oPaginate": {
+	"sFirst":    "Pierwsza",
+	"sPrevious": "Poprzednia",
+	"sNext":     "Następna",
+	"sLast":     "Ostatnia"
+	},
+    "sEmptyTable":     "Brak danych",
+    "sLoadingRecords": "Wczytywanie...",
+    "oAria": {
+	"sSortAscending":  ": aktywuj, by posortować kolumnę rosnąco",
+	"sSortDescending": ": aktywuj, by posortować kolumnę malejąco"
+	}
+	    },
+        "aaSorting": [[0,'desc']]
+      } );
+    });
+   </script>
 </div>
